f7923f3657076789ad76ae7e8b8e15231b0ec346
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-22 09:30:00 -0400

    Upload 22/7

---
+++ BoolTest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BoolTest {
+    public static void main (String[] args){
+        //create object
+        Scanner input = new Scanner(System.in);
+
+        //get boolean value
+        boolean value = input.nextBoolean();
+
+        //check the value
+        System.out.println("Value is:" +value);
+
+
+    }
+}
---
+++ BooleanEx.java
@@ -0,0 +1,12 @@
+public class BooleanEx {
+    public static void main (String[]args){
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = (b1!=b2); //Equality Comparison
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+
+    }
+}
