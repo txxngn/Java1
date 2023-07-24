c7073636c9b454e9f12f0c282c7f3b93889979cd
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-24 13:51:10 -0400

    Upload 24/7

---
+++ increment.java
@@ -0,0 +1,32 @@
+public class increment {
+    public static void main(String[] args) {
+       /*int x = 2;
+
+       int a = x++;
+       System.out.println(x);
+       System.out.println(a);
+
+       int y = 3;
+       int b = ++y;
+       System.out.println(y);
+       System.out.println(b);*/
+
+        /*int i = 10;
+        int newnum = 10 * i++;
+        System.out.println(newnum);
+        System.out.println(i);
+
+        int e = 10;
+        int numnew = 10 * (++e);
+        System.out.println(numnew);
+        System.out.println(e);*/
+
+        int j = 10;
+        int k = j++%5;
+        System.out.println(j);
+        System.out.println(k);
+
+
+
+    }
+}
