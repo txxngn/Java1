a2a85e9c1eed4f016fec8ec46233db46fe6cf47b
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-26 18:52:17 -0400

    Upload 26/7

---
+++ While2.java
@@ -0,0 +1,31 @@
+public class While2 {
+    public static void main (String [] args){
+
+        int start = 1;
+        int end = 5;
+        int number = start;
+
+        while (number <= end){
+            System.out.println(number);
+            number++;
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
