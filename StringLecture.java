8f218c555d17d2673dbb97fb96d9be24ad529522
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-26 18:52:06 -0400

    Upload 26/7

---
+++ StringLecture.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.lang.String;
+
+
+public class StringLecture {
+    public static void main (String[] args){
+/*        String message = "Welcome to java";
+        System.out.println(message.length());   //Give number of length of the string, counted from 1
+        System.out.println(message.charAt(8));   //Pick-out the character of corresponding number, counted from 0
+*/
+        //array = group of character - like string
+
+        //next và nextLine()
+/*        Scanner input = new Scanner (System.in);
+        System.out.println("Enter three words separated by spaces: ");
+        String s1 = input.nextLine();                                //nextLine() read full string
+        String s2 = input.next();                                    //next() read string to the space and stop,
+        String s3 = input.nextLine();                                //store rest of the string to the memory
+        String s4 = input.next();
+
+        System.out.println("s1 is " + s1);
+        System.out.println("s2 is " + s2);
+        System.out.println("s3 is "+ s3);
+        System.out.println("s4 is "+ s4);
+        char ch = s1.charAt(0);
+        System.out.println("The character entered is " +ch);
+*/
+
+        //Method of String
+        /*String S1 = "Welcome";
+        String S2 = "Class";
+        System.out.println(S1.concat(S2));    //Method to concatinate two strings
+        int result = S1.compareTo(S2);
+        System.out.println(result);
+
+        if (result == 0)
+            System.out.println("Two strings are equal");
+        else
+            System.out.println("Two strings are not equal");*/
+
+        /*String A = "Welcome";
+        //Converts String to lower case
+        String B = A.toLowerCase();
+        System.out.println(B);
+        //convert string to upper case
+        System.out.println(A.toUpperCase());*/
+
+
+
+        /*S2 = S1.replace('o', 'a');   //replaces character x with y and save it in s2
+        System.out.println(S2);
+
+        S1 = "                          OK   ";
+        System.out.println(s1);
+
+        S2 = S1.trim();         // removes spaces from befinning and end of the string
+        System.out.println(S2);
+
+        String S6 = "     Home   Welcome    ";
+        System.out.println(S6.trim()); */
+
+
+
+
+
+
+    }
+}
