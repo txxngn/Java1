b662a94a49f235a5426d91f2e98ed14cf6e01810
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-27 11:13:42 -0400

    Upload 27/7

---
+++ Prac1W7.java
@@ -0,0 +1,27 @@
+package Week7;
+import  java.util.Scanner;
+
+public class Prac1W7 {
+    public static void main (String [] args){
+        Scanner input = new Scanner (System.in);
+
+
+        int ans;
+
+        do{
+            System.out.println("What is 3+4? ");
+            ans = input.nextInt();
+            if (ans == 7) {
+                System.out.println("Correct");
+                break;
+            }
+            else
+                System.out.println("Incorrect");
+
+        }while(ans != 7);
+
+
+
+
+    }
+}
