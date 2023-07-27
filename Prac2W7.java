1893757d0f666acecdaf12d2365a502ce9847a89
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-27 11:13:52 -0400

    Upload 27/7

---
+++ Prac2W7.java
@@ -0,0 +1,34 @@
+package Week7;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Prac2W7 {
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        /*
+        WAP that randomly generates an integer between 0 and 100, inclusive.
+        The program prompts the user to enter a number continuously until the number matches the randomly generated number.
+        For each user input, the program tells the user whether the input is too low or too high,
+        so the user can choose the next input intelligently
+         */
+
+
+        //run random num
+        double ran_num = Math.random() * 100;
+        int type_cast = (int) ran_num;
+        System.out.println (type_cast);
+
+        //Ask for user input
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+
+        //Condition
+        if (num == type_cast)
+            System.out.println("OK");
+        else if (num > type_cast)
+            System.out.println("too big");
+        else
+            System.out.println("Too low");
+    }
+}
