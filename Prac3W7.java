1fa13d6b1071c9e92a8e9e83590c2ae87d00d657
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-27 11:14:05 -0400

    Upload 27/7

---
+++ Prac3W7.java
@@ -0,0 +1,29 @@
+package Week7;
+import java.util.Scanner;
+
+public class Prac3W7 {
+    public static void main (String [] args){
+        Scanner input = new Scanner (System.in);
+
+        int num_try = 0;
+        int grade;
+
+        do{
+            System.out.println("Enter the final grade: ");
+            grade = input.nextInt();
+            if (grade > 0 && grade < 100){
+                System.out.println("You entered a valid grade.");
+                break;
+            }
+            else {
+                System.out.println("You entered invalid grade.");
+                num_try++;
+            }
+
+
+        }while ((grade > 0 || grade < 100) && num_try < 5 && grade != 999);
+
+        
+
+    }
+}
