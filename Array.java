d0caad3a0e0d6bbf787aa52bb6c712266fc23651
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-29 09:25:54 -0400

    Upload 29/7

---
+++ Array.java
@@ -0,0 +1,14 @@
+package Week9;
+
+public class Array {
+    public static void main (String [] args){
+        int grades[] = {40,50,44,84,92,32};    //variable phải có [] to determine this variable is an array
+        System.out.println(grades[0]);         //array is A VARIABLE that can hold multiple values
+        System.out.println(grades[1]);
+        System.out.println(grades[2]);
+        System.out.println(grades[3]);
+        System.out.println(grades[4]);
+        System.out.println(grades[5]);
+
+    }
+}
