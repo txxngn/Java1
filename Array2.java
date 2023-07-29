6bf93f7b70e53f1bcd953ea823595a54086bde78
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-29 09:26:07 -0400

    Upload 29/7

---
+++ Array2.java
@@ -0,0 +1,28 @@
+package Week9;
+
+public class Array2 {
+    public static void main (String [] args){
+
+        double price [] = new double[4];    //this array has data type is double
+        price [0] = 23.1;                   //"new double" is syntax, mean this array has 4 elements
+        price [1] = 12.4;                   //and then we initialize the value for each index of the array
+        price [2] = 14.31;
+        price [3] = 21.5;
+
+        System.out.println(price[0]);
+        System.out.println(price[1]);
+        System.out.println(price[2]);
+        System.out.println(price[0]);
+
+        int x[] = new int[3];
+        x[0] = 2;
+        x[1] = 4;
+        x[2] = 5;
+        System.out.println(x[2]);
+        System.out.println(x[1]);
+
+
+
+
+    }
+}
