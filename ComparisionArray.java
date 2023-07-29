b2198eb9ef4a31b24f3581e8f3095af379a6ad6a
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-29 09:26:50 -0400

    Upload 29/7

---
+++ ComparisionArray.java
@@ -0,0 +1,17 @@
+package Week9;
+import java.util.Arrays;                               //Phải import Arrays trước!!!!!!
+
+public class ComparisionArray {
+    public static void main (String [] args){
+        int Arr1[] = {3,65,21,15};
+        int Arr2[] = {3,65,21,1,57,87};
+
+        //Compare 2 arrays (Give True or False)
+        System.out.println("Integer Arrays on comparison: " + Arrays.equals(Arr1,Arr2));
+
+
+
+
+
+    }
+}
