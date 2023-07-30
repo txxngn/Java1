cc1beebca13ca62d49c94d3dae2d0ac733f91ff1
Thai Nguyen <125842205+txxngn@users.noreply.github.com>
2023-07-30 10:10:27 -0400

    Upload 30/7

---
+++ ForEachLoop.java
@@ -0,0 +1,17 @@
+package Week9;
+
+public class ForEachLoop {
+    public static void main (String [] args){
+        /*
+        FASTER WAY TO LOOP AN ARRAY
+         */
+        int array[] = {2,3,32,47};
+        //reading the array using "for-each" loop
+        for (int i:array){
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+}
